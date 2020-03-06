f470a17c94183c5b242d70954a7bc49f4be24483
vinceliu21 <vinceliu21@gmail.com>
2020-03-06 13:05:17 -0500

    heroku deployment

---
+++ build/precache-manifest.b3c6a916d76d2b4793a7c3263b61dd52.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3225505319ef57a3e69cb430b1b20947",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5d574525381dea60572",
+    "url": "/static/css/main.d83e7cb1.chunk.css"
+  },
+  {
+    "revision": "176291752bd5a6e568f3",
+    "url": "/static/js/2.4530c5d1.chunk.js"
+  },
+  {
+    "revision": "8208df2167b316a10b7cb2b34472ec7b",
+    "url": "/static/js/2.4530c5d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5d574525381dea60572",
+    "url": "/static/js/main.9fe37732.chunk.js"
+  },
+  {
+    "revision": "08a01cac7a9424eef63f",
+    "url": "/static/js/runtime-main.ef76e641.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
