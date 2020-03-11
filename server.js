cb02036ba0fe5264113239c41a8bc38416b150cc
vinceliu21 <vinceliu21@gmail.com>
2020-03-11 08:59:29 -0400

    server bugs

--- server.js
+++ server.js
@@ -15,30 +15,6 @@ const readStream = request.get("https://raw.githubusercontent.com/CSSEGISandData
 
 var usa_cases_ts = {};
 
-getCSV('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv')
-.then(rows => {
-
-  let us = [];
-  for (var i = 0; i < rows.length; i++){
-    if (rows[i]['Country/Region'] == 'US'){
-      delete rows[i]['Country/Region'];
-      delete rows[i]['Province/State'];
-      delete rows[i]['Lat'];
-      delete rows[i]['Long'];
-      us.push(rows[i]);
-      for (var key in rows[i]){
-        if (!(key in usa_cases_ts)){
-          usa_cases_ts[key] = parseInt(rows[i][key]);
-        } else{
-          usa_cases_ts[key] += parseInt(rows[i][key]);
-        }
-      }
-    }
-  }
-  console.log(usa_cases_ts);
-
-});
-
 
 
 // var usa_cases_data_ts = {"1/22/20":"0","1/23/20":"0","1/24/20":"0","1/25/20":"0","1/26/20":"0","1/27/20":"1","1/28/20":"1","1/29/20":"1","1/30/20":"1","1/31/20":"1","2/1/20":"1","2/2/20":"1","2/3/20":"1","2/4/20":"1","2/5/20":"1","2/6/20":"1","2/7/20":"1","2/8/20":"1","2/9/20":"1","2/10/20":"1","2/11/20":"1","2/12/20":"1","2/13/20":"1","2/14/20":"1","2/15/20":"1","2/16/20":"1","2/17/20":"1","2/18/20":"1","2/19/20":"1","2/20/20":"1","2/21/20":"1","2/22/20":"1","2/23/20":"1","2/24/20":"1","2/25/20":"1","2/26/20":"1","2/27/20":"1","2/28/20":"1","2/29/20":"6","3/1/20":"9","3/2/20":"14","3/3/20":"21","3/4/20":"31","3/5/20":"51","3/6/20":"58","3/7/20":"71","3/8/20":"83"};
@@ -92,40 +68,65 @@ cron.schedule("30 * * * *", function() {
       }
 
 
-      getCSV('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv')
-      .then(rows => {
-    
-        let us = [];
-        usa_cases_ts = {};
-        for (var i = 0; i < rows.length; i++){
-          if (rows[i]['Country/Region'] == 'US'){
-            delete rows[i]['Country/Region'];
-            delete rows[i]['Province/State'];
-            delete rows[i]['Lat'];
-            delete rows[i]['Long'];
-            us.push(rows[i]);
-            for (var key in rows[i]){
+  getCSV('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv')
+  .then(rows => {
+
+    let us = [];
+    usa_cases_ts = {};
+    var varDate = new Date('03-10-2020'); //dd-mm-YYYY
+    varDate.setHours(0,0,0,0);
+    var today = new Date();
+    today.setHours(0,0,0,0);
+
+    for (var i = 0; i < rows.length; i++){
+      if (rows[i]['Country/Region'] == 'US'){
+        delete rows[i]['Country/Region'];
+        delete rows[i]['Lat'];
+        delete rows[i]['Long'];
+        
+        // delete rows[i]['Province/State'];
+        for (var key in rows[i]){
+          if (key == 'Province/State'){continue;}
+          var temp = new Date(key);
+          temp.setHours(0,0,0,0);
+          if (temp < varDate){
+            if(rows[i]['Province/State'].includes(",")){
               if (!(key in usa_cases_ts)){
                 usa_cases_ts[key] = parseInt(rows[i][key]);
               } else{
                 usa_cases_ts[key] += parseInt(rows[i][key]);
               }
+
+            }
+
+          }else{
+            if (!rows[i]['Province/State'].includes(",")){
+              if (!(key in usa_cases_ts)){
+                usa_cases_ts[key] = parseInt(rows[i][key]);
+              } else{
+                usa_cases_ts[key] += parseInt(rows[i][key]);
+              }
+
             }
           }
         }
-        var today = new Date();
-        var dd = today.getDate();
+      }
+    }
 
-        var mm = today.getMonth()+1; 
-        var yyyy = '20';
+    var today = new Date();
+    var dd = today.getDate();
 
-        today = mm+'/'+dd+'/'+yyyy;
-        console.log(today);
+    var mm = today.getMonth()+1; 
+    var yyyy = '20';
 
-        usa_cases_ts[today] = result.USA.total_cases;
+    today = mm+'/'+dd+'/'+yyyy;
+    console.log(today);
 
-        console.log(usa_cases_ts);
-      });
+    var result1 = result.USA.total_cases.replace(/,/g, "");
+    console.log(parseInt(result1));
+    usa_cases_ts[today] = result1;
+
+  });
 
       // Closing the Puppeteer controlled headless browser
       await browser.close();
@@ -179,23 +180,47 @@ app.get('/api', function(req, res) {
           result[covid_countries[i]] = {total_cases: covid_total_cases[i], total_deaths: covid_total_deaths[i]};
         }
 
+
         getCSV('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv')
         .then(rows => {
       
           let us = [];
           usa_cases_ts = {};
+          var varDate = new Date('03-10-2020'); //dd-mm-YYYY
+          varDate.setHours(0,0,0,0);
+          var today = new Date();
+          today.setHours(0,0,0,0);
+      
           for (var i = 0; i < rows.length; i++){
             if (rows[i]['Country/Region'] == 'US'){
               delete rows[i]['Country/Region'];
-              delete rows[i]['Province/State'];
               delete rows[i]['Lat'];
               delete rows[i]['Long'];
-              us.push(rows[i]);
+              
+              // delete rows[i]['Province/State'];
               for (var key in rows[i]){
-                if (!(key in usa_cases_ts)){
-                  usa_cases_ts[key] = parseInt(rows[i][key]);
-                } else{
-                  usa_cases_ts[key] += parseInt(rows[i][key]);
+                if (key == 'Province/State'){continue;}
+                var temp = new Date(key);
+                temp.setHours(0,0,0,0);
+                if (temp < varDate){
+                  if(rows[i]['Province/State'].includes(",")){
+                    if (!(key in usa_cases_ts)){
+                      usa_cases_ts[key] = parseInt(rows[i][key]);
+                    } else{
+                      usa_cases_ts[key] += parseInt(rows[i][key]);
+                    }
+      
+                  }
+      
+                }else{
+                  if (!rows[i]['Province/State'].includes(",")){
+                    if (!(key in usa_cases_ts)){
+                      usa_cases_ts[key] = parseInt(rows[i][key]);
+                    } else{
+                      usa_cases_ts[key] += parseInt(rows[i][key]);
+                    }
+      
+                  }
                 }
               }
             }
@@ -209,9 +234,11 @@ app.get('/api', function(req, res) {
           today = mm+'/'+dd+'/'+yyyy;
           console.log(today);
 
-          usa_cases_ts[today] = result.USA.total_cases;
-
-          console.log(usa_cases_ts);
+          var result1 = result.USA.total_cases.replace(/,/g, "");
+          console.log(parseInt(result1));
+          usa_cases_ts[today] = result1;
+      
+      
         });
 
         // Closing the Puppeteer controlled headless browser
@@ -242,22 +269,46 @@ app.get('/csv', async (req, res) => {
 
     let us = [];
     usa_cases_ts = {};
+    var varDate = new Date('03-10-2020'); //dd-mm-YYYY
+    varDate.setHours(0,0,0,0);
+    var today = new Date();
+    today.setHours(0,0,0,0);
+
     for (var i = 0; i < rows.length; i++){
       if (rows[i]['Country/Region'] == 'US'){
         delete rows[i]['Country/Region'];
-        delete rows[i]['Province/State'];
         delete rows[i]['Lat'];
         delete rows[i]['Long'];
-        us.push(rows[i]);
+        
+        // delete rows[i]['Province/State'];
         for (var key in rows[i]){
-          if (!(key in usa_cases_ts)){
-            usa_cases_ts[key] = parseInt(rows[i][key]);
-          } else{
-            usa_cases_ts[key] += parseInt(rows[i][key]);
+          if (key == 'Province/State'){continue;}
+          var temp = new Date(key);
+          temp.setHours(0,0,0,0);
+          if (temp < varDate){
+            if(rows[i]['Province/State'].includes(",")){
+              if (!(key in usa_cases_ts)){
+                usa_cases_ts[key] = parseInt(rows[i][key]);
+              } else{
+                usa_cases_ts[key] += parseInt(rows[i][key]);
+              }
+
+            }
+
+          }else{
+            if (!rows[i]['Province/State'].includes(",")){
+              if (!(key in usa_cases_ts)){
+                usa_cases_ts[key] = parseInt(rows[i][key]);
+              } else{
+                usa_cases_ts[key] += parseInt(rows[i][key]);
+              }
+
+            }
           }
         }
       }
     }
+
     console.log(usa_cases_ts);
     res.send({ok: usa_cases_ts});
 
